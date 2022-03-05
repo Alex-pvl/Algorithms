7abb24345e69ce0b54136bae107cd99190cc1695
Alexandr Pavlyuk <sasha.pavlyuk.1996@inbox.ru>
2022-03-05 14:59:15 +0700

    init

---
+++ Laba-1/List.h
@@ -0,0 +1,54 @@
+#pragma once
+
+template <class T>
+class List
+{
+public:
+	
+	List();
+	List(const List<T>&);
+	~List();
+	int getSize();
+	void clear();
+	bool isEmpty();
+	bool hasObject(T);
+	T getObject(int);
+	bool editObject(T, int);
+	int getPosition(T);
+	void add(T);
+	bool add(T, int);
+	bool remove(T);
+	bool remove(int);
+
+	class Node {
+	public:
+		Node();
+		Node(T);
+		~Node();
+		T object;
+		Node* next;
+	};
+
+	class Iterator {
+	public:
+		Iterator(List&) {
+
+		}
+		T& operator *();
+		Iterator operator++();
+		bool operator==(Iterator&);
+		bool operator!=(Iterator&);
+	private:
+		List* l;
+		Node* cur;
+	};
+	Iterator begin();
+	Iterator end();
+
+	friend class Node;
+	friend class Iterator;
+private:
+	Node* head;
+	int size;
+};
+

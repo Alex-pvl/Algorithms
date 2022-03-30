4e8d524829ac5491755c810a87a61bb984ba25df
Alexandr Pavlyuk <sasha.pavlyuk.1996@inbox.ru>
2022-03-30 15:55:17 +0700

    v.1.1

--- Laba-1/List.cpp
+++ Laba-1/List.cpp
@@ -3,19 +3,26 @@
 
 using namespace std;
 
+
+// --------------------- Итератор ------------------------------
 template <class T>
 List<T>::Iterator::Iterator(List& list) {
-	this->l = list;
+	this->l = &list;
 	this->cur = list.head;
 }
 
 template <class T>
 T& List<T>::Iterator::operator*() { //Операция доступа по значению теперь возвращает NULL, если итератор равен NULL (возможно стоило бы бросать исключения)
-	if (this->cur != nullptr) {
-		return this->cur->object;
+	try {
+		if (this->cur != nullptr) {
+			return this->cur->object;
+		}
+		else {
+			throw exception("Отказ доступа"); // что?
+		}
 	}
-	else {
-		return nullptr;
+	catch (const exception e) {
+		cerr << e.what();
 	}
 }
 
@@ -24,7 +31,7 @@ typename List<T>::Iterator List<T>::Iterator::operator++() {
 	try {
 		if (this->cur != nullptr) {
 			this->cur = this->cur->next;
-			return this;
+			return *this;
 		}
 		else {
 			throw exception("Список пуст"); //Добавлено исключение при значении объекта итератора равном NULL (но нужно ли оно?)
@@ -47,16 +54,37 @@ bool List<T>::Iterator::operator!=(Iterator& iter) {
 
 template <class T>
 typename List<T>::Iterator List<T>::begin() {
-	Iterator beg(this);
+	Iterator beg(*this);
 	return beg;
 }
 
 template <class T>
 typename List<T>::Iterator List<T>::end() {
-	Iterator end(this);
+	Iterator end(*this);
 	end.cur = nullptr;
 	return end;
 }
+
+// --------------------- Узел ------------------------------
+
+template <class T> 
+List<T>::Node::Node() {
+	this->object = nullptr; // new T
+	this->next = nullptr;
+}
+
+template <class T>
+List<T>::Node::Node(T object) {
+	this->object = object; 
+	this->next = nullptr;
+}
+
+template <class T>
+List<T>::Node::~Node() {
+	delete this->object; 
+}
+
+// --------------------- Список ------------------------------
 
 template <class T>
 List<T>::List() {
@@ -67,22 +95,24 @@ List<T>::List() {
 template <class T>
 List<T>::List(const List<T>& list) {
 	this->size = list->size;
-	Iterator iter(list);
-	iter = list.begin();
-	if (iter != list.end()) {
-		this->head = new Node(*iter); //Надо тестить
-		Node prev = this->head;
-		++iter;
-		while (iter != list.begin())
-		{
-			Node* cur = new Node(*iter);
-			prev.next = cur; //???
-			prev = cur;
-			delete cur;
-			++iter;
-		}
-		prev.next = this->head;
-	}
+	
+	//this->size = list->size;
+	//Iterator iter(list);
+	//iter = list.begin();
+	//if (iter != list.end()) {
+	//	this->head = new Node(*iter); //Надо тестить
+	//	Node prev = this->head;
+	//	++iter;
+	//	while (iter != list.begin())
+	//	{
+	//		Node* cur = new Node(*iter);
+	//		prev.next = cur; //???
+	//		prev = cur;
+	//		delete cur;
+	//		++iter;
+	//	}
+	//	prev.next = this->head;
+	//}
 }
 
 template <class T>
@@ -97,43 +127,38 @@ int List<T>::getSize() {
 
 template <class T>
 void List<T>::clear() {
-	Iterator iter(this);
-	for (int i = 0; i < this->size; i++){
-		Node* del = iter.cur; //???
-		++iter; //Под подозрением -_-
-		delete del;
-	}
+	//Iterator iter(*this);
+	//for (int i = 0; i < this->size; i++){
+	//	Node* del = iter.cur; //???
+	//	++iter; //Под подозрением -_-
+	//	delete del;
+	//}
 }
 
 template <class T>
 bool List<T>::isEmpty() {
-	return !size; //Pizdec
+	return size == 0; 
 }
 
 template <class T>
 bool List<T>::hasObject(T object) {
-	Iterator iter(this);
-	iter = this->begin();
-	for (int i = 0; i < this->size; i++){
-		if (*(*iter) == object) { //????
-			return true;
-		}
-	}
+	//Iterator iter(*this);
+	//iter = this->begin();
+	//for (int i = 0; i < this->size; i++){
+	//	if ((*iter) == object) { //????
+	//		return true;
+	//	}
+	//}
 	return false;
 }
 
 template <class T>
 T List<T>::getObject(int n) {
 	try {
-		if (n > size || n < 1) {
-			throw exception("Индекс вне размера списка");
-		}
-		else {
-			Iterator iter(this);
-			iter = this->begin();
-			for (int i = 1; i < n; i++, ++iter);
-			return *(*iter); //????
-		}
+		if (n > size || n < 1) throw exception("Некорректный индекс");
+		Node* tmp = this->head;
+		for (int i = 0; i < n - 1; i++) tmp = tmp->next;
+		return *(tmp->object);
 	}
 	catch (const exception e) {
 		cerr << e.what();
@@ -141,7 +166,7 @@ T List<T>::getObject(int n) {
 }
 
 template <class T>
-bool List<T>::editObject(T, int) {
+bool List<T>::editObject(T object, int index) {
 	// TODO
 	return false;
 }
@@ -149,13 +174,13 @@ bool List<T>::editObject(T, int) {
 template <class T>
 int List<T>::getPosition(T object) {
 	Node* tmp = this->head;
-	Iterator iter(this);
-	iter = this->begin();
-	for (int i = 0; i < size; i++) {
-		if (*(*iter) == object) {
-			return i;
+	int index = 0;
+	while (tmp->next != this->head) {
+		if (tmp->object == object) {
+			return index + 1; // надо инкремент?
 		}
-		++iter;
+		index++;
+		tmp = tmp->next;
 	}
 	return -1;
 }
@@ -176,3 +201,21 @@ void List<T>::add(T object) {
 	}
 }
 
+template <class T>
+bool List<T>::add(T object, int index) {
+	// TODO
+	return false;
+}
+
+
+template <class T>
+bool List<T>::remove(T object) {
+	// TODO
+	return false;
+}
+
+template <class T>
+bool List<T>::removeAt(int index) {
+	// TODO
+	return false;
+}
